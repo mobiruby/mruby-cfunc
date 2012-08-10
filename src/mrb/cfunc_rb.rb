46ef3d859c08a57515a1443b1d9147dc2aaf7863
Yuichiro MASUI <masui@masuidrive.jp>
2012-08-11 01:21:57 +0900

    Added Struct.define arugment

--- src/mrb/cfunc_rb.rb
+++ src/mrb/cfunc_rb.rb
@@ -102,6 +102,7 @@ class CFunc::Struct
         attr_accessor :elements, :size, :align
         
         def define(*args)
+            args = args.first if args.is_a?(Array) && args.size == 0
             @elements = []
             types, offset = [], 0
             max_align = 0
