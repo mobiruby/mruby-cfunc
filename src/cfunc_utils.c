bb31111a752e82d74872a4f7c0204357ac9ddbdb
Yuichiro MASUI <masui@masuidrive.jp>
2012-11-17 15:44:29 +0900

    Changed indent

--- src/cfunc_utils.c
+++ src/cfunc_utils.c
@@ -15,15 +15,15 @@
 void
 cfunc_mrb_raise_without_jump(mrb_state *mrb, struct RClass *c, const char *fmt, ...)
 {
-  va_list args;
-  char buf[256];
-  int n;
+    va_list args;
+    char buf[256];
+    int n;
 
-  va_start(args, fmt);
-  n = vsnprintf(buf, sizeof(buf), fmt, args);
-  va_end(args);
-  if (n < 0) {
-    n = 0;
-  }
-  mrb->exc = (struct RObject*)mrb_object(mrb_exc_new(mrb, c, buf, n));
+    va_start(args, fmt);
+    n = vsnprintf(buf, sizeof(buf), fmt, args);
+    va_end(args);
+    if (n < 0) {
+      n = 0;
+    }
+    mrb->exc = (struct RObject*)mrb_object(mrb_exc_new(mrb, c, buf, n));
 }
