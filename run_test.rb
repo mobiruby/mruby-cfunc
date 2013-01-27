ad6d4dbf1d1ef8c44b6cd68d72fb636cf90a82b9
Yuichiro MASUI <masui@masuidrive.jp>
2013-01-28 02:03:55 +0900

    Fixed test build script for CRuby 1.8

--- run_test.rb
+++ run_test.rb
@@ -6,7 +6,7 @@ if __FILE__ == $PROGRAM_NAME
   unless File.exists?('tmp/mruby')
     system 'git clone https://github.com/mruby/mruby.git tmp/mruby'
   end
-  exit system(%Q[cd tmp/mruby; MRUBY_CONFIG=#{File.expand_path __FILE__} rake #{ARGV.join(' ')}])
+  exit system(%Q[cd tmp/mruby; MRUBY_CONFIG=#{File.expand_path __FILE__} ./minirake #{ARGV.join(' ')}])
 end
 
 MRuby::Build.new do |conf|
