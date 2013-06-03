d5cb64ccb24d48d4d0ab57395f6b1d87bcfe66d6
Julien Ammous <schmurfy@gmail.com>
2013-06-03 11:28:41 +0200

    added failing test

--- test/string1.rb
+++ test/string1.rb
@@ -4,4 +4,10 @@ mobiruby_test "CFunc::String1" do
 
   CFunc::call(CFunc::Void, "strcpy", ptr, str)
   assert_equal "STRING", ptr.to_s
+  
+  
+  ptr2 = CFunc::UInt8[4]
+  CFunc::call(CFunc::Void, "strcpy", ptr2, "too long")
+  assert_equal "too ", ptr2.to_s
+  
 end
