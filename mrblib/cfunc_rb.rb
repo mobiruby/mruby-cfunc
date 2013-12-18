014ba190367ca2b95170960db4b1cd94d8f9b5c0
ppibburr <tulnor33@gmail.com>
2013-12-17 22:07:31 -0500

    Document & CFunc::Struct.refer will raise when called on CFunc::Struct

--- mrblib/cfunc_rb.rb
+++ mrblib/cfunc_rb.rb
@@ -9,16 +9,59 @@ class CFunc::Type
 end
 
 module CFunc
+    # 
+    # @param [Integer] val the number to represent
+    # @return [CFunc::Int] representing +val+
     def self.Int(val); Int.new(val) end
+    
+    #
+    # @param [Integer] val the number to represent
+    # @return [CFunc::UInt8] representing +val+
     def self.UInt8(val); UInt8.new(val) end
+
+    #
+    # @param [Integer] val the number to represent
+    # @return [CFunc::SInt8] representing +val+    
     def self.SInt8(val); SInt8.new(val) end
+    
+    #
+    # @param [Integer] val the number to represent
+    # @return [CFunc::UInt16] representing +val+
     def self.UInt16(val); UInt16.new(val) end
+    
+    #
+    # @param [Integer] val the number to represent
+    # @return [CFunc::SInt16] representing +val+    
     def self.SInt16(val); SInt16.new(val) end
+    
+    #
+    # @param [Integer] val the number to represent
+    # @return [CFunc::UInt32] representing +val+    
     def self.UInt32(val); UInt32.new(val) end
+    
+    #
+    # @param [Integer] val the number to represent
+    # @return [CFunc::SInt32] representing +val+    
     def self.SInt32(val); SInt32.new(val) end
+    
+    #
+    # @param [Integer] val the number to represent
+    # @return [CFunc::UInt64] representing +val+    
     def self.UInt64(val); UInt64.new(val) end
+    
+    #
+    # @param [Integer] val the number to represent
+    # @return [CFunc::SInt64] representing +val+    
     def self.SInt64(val); SInt64.new(val) end
+    
+    #
+    # @param [Numeric] val the number to represent
+    # @return [CFunc::Float] representing +val+    
     def self.Float(val); Float.new(val) end
+    
+    #
+    # @param [Numeric] val the number to represent
+    # @return [CFunc::Double] representing +val+    
     def self.Double(val); Double.new(val) end
 
     class Type
@@ -44,9 +87,13 @@ end
 
 
 module CFunc
+    # A pointer to a function
     class FunctionPointer < Pointer
         attr_accessor :result_type, :arguments_type
 
+        #
+        # @param [void] args swarm of arguments (if any) to pass to the function
+        # @return [::Object] the result of calling the function
         def call(*args)
             @result_type ||= CFunc::Void
             @arguments_type.each_with_index do |arg_type, idx|
@@ -58,6 +105,12 @@ module CFunc
         end
     end
 
+    # Stores a function
+    # @see(CFunc::[])
+    #
+    # @param [CFunc::Type] result_type the type of the result of calling the function
+    # @param [String] funcname the name of the function (note: the name must be of a symbol in `@dlh` or from a library that `@dlh` is linked against)
+    # @param [void] args swarm of CFunc::Type's (if any) of the argument types.
     def self.define_function(result_type, funcname, *args)
         @dlh ||= CFunc::call(CFunc::Pointer, :dlopen, nil, nil)
         @dlsym_func ||= CFunc::call(CFunc::Pointer, :dlsym, @dlh, "dlsym")
@@ -70,11 +123,15 @@ module CFunc
         @funcptrs[funcname.to_sym] = funcptr
     end
 
+    #
+    #
+    # @return [CFunc::FunctionPointer] mapped to +funcname+. returns `nil` if no such mapping.
     def self.[](funcname)
         @funcptrs[funcname.to_sym]
     end
 end
 
+# A pointer
 class CFunc::Pointer
     class << self
         def type
@@ -87,6 +144,7 @@ class CFunc::Pointer
         end
     end
 
+   
     def refer
         self.class.type.refer(self)
     end
@@ -105,7 +163,11 @@ class String
     def self.to_ffi_type
         CFunc::Pointer(CFunc::UInt8).to_ffi_type
     end
-
+    
+    # Get a String from a pointer
+    #
+    # @param [CFunc::Pointer] ptr
+    # @return [String] the string value of +ptr+
     def self.refer(ptr)
         CFunc::Pointer.refer(ptr).to_s
     end
@@ -118,6 +180,10 @@ end
 
 $klasses = []
 module CFunc
+    # Creates a class subclassing CFunc::Pointer, and sets it +type+
+    #
+    # @param [CFunc::Type] type the type of the pointer
+    # @return [::Class]
     def self.Pointer(type)
         return Pointer if Void == type
         klass = ::Class.new(Pointer)
@@ -127,9 +193,13 @@ module CFunc
     end
 end
 
+# Wraps a pointer of an array
 class CFunc::CArray < CFunc::Pointer
     attr_accessor :size
 
+    #
+    # @param [Integer] size the size of the array
+    # @return [CFunc::CArray]
     def self.new(size)
         obj = self.malloc(type.size * size)
         obj.size = size
@@ -138,20 +208,48 @@ class CFunc::CArray < CFunc::Pointer
 end
 
 module CFunc
+    # Creates a class subclassing CFunc::CArray whose `type` is +type+ 
+    #
+    # @param [CFunc::Type] type the type of the array
+    # @return [::Class]
     def self.CArray(type)
         klass = ::Class.new(CArray)
         klass.type = type
         klass
     end
 
+    # Convienience, is CFunc::SInt32
     class Int < SInt32
     end
 end
 
+# @abstract Implements wrapping a struct
+# Must be subclassed (either directly or as the result of CFunc::Struct.define())
+#
+# @example
+#   =begin
+#     typedef struct {
+#       int    bar;
+#       double quux;
+#     } Foo;
+#   =end
+#
+#   class Foo < CFunc::Struct
+#     define CFunc::Int, :bar,
+#            CFunc::Double, :quux   
+#   end
 class CFunc::Struct
     class << self
         attr_accessor :elements, :size, :align
-        
+    
+        # Defines the fields and thier types. 
+        # Returns a subclass of CFunc::Struct OR if called on a subclass, the subclass
+        #
+        # @example
+        #   Foo.define CFunc::Int, :bar, CFunc::Double, :quux
+        #
+        # @param [void] args the layout
+        # @return [::Class] the resulting class    
         def define(*args)
             args = args.first if args.is_a?(Array) && args.size == 1
             @elements = []
@@ -191,6 +289,13 @@ class CFunc::Struct
         end
     end
 
+    # When passed a CFunc::Pointer we will wrap that pointer
+    # When passed a Hash we will create the pointer we wrap, its size will be the size of the struct multiplied by the value of the `:size` member of the hash
+    # When passed nil or when no arguments are passed we create the pointer we wrap
+    #
+    # @raise [RuntimeError] when passed something we cant handle
+    #
+    # @param [void] val
     def initialize(val=nil)
         if val == nil
             @pointer = CFunc::Pointer.malloc(self.class.size)
@@ -201,19 +306,37 @@ class CFunc::Struct
             @pointer = CFunc::Pointer.malloc(self.class.size * val[:size])
             @pointer.autofree
         else
-            raise "Unknow"
+            raise "Do not know how to initialize the structure from #{val}"
         end
     end
 
+    # Wrap a pointer
+    # 
+    # @param [CFunc::Pointer] pointer the pointer to wrap
+    # @return [CFunc::Struct] the instance of the class being called on
     def self.refer(pointer)
+        if self == CFunc::Struct
+          raise "CFunc::Struct cannot be used directly"
+        end
+    
         self.new(pointer)
     end
     
+    # Retieve the value of member +fieldname+
+    #
+    # @param [Symbol] fieldname the name of the field to retrive
+    # @return [CFunc::Type] the value
     def element(fieldname)
         field = lookup(fieldname)
         field[0].refer(@pointer.offset(field[2]))
     end
     
+    # Retrieves the value of +fieldname+
+    # if the CFunc::Type for that field responds to `value`
+    # then the result of `value` called on that CFunc::Type is returned
+    #
+    # @param [Symbol] fieldname the name of the field to retrieve
+    # @return [Object, CFunc::Type] the value
     def [](fieldname)
         field = lookup(fieldname)
         if field[3] == :new
@@ -224,12 +347,19 @@ class CFunc::Struct
         end
     end
 
+    # Sets a fields value
+    # the fields CFunc::Type must respond to `value=`
+    #
+    # @param [Symbol] fieldname the name of the field to set
+    # @param [Object] val the value to set
+    # @return [void]
     def []=(fieldname, val)
         field = lookup(fieldname)
         el = field[0].refer(@pointer.offset(field[2]))
         el.value = val
     end
 
+    # @return [CFunc::Pointer] the pointer we wrap
     def addr
         @pointer
     end
